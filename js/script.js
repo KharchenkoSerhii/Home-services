3ebcab2e20b1e0817bca5cf58e261c7d64a6891b
Serhii Kharchenko <kalymar7k@gmail.com>
2023-12-29 02:51:46 +0200

    end section Hero

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 'use ctrict';
 
+// функция ibg=======================================================
+function ibg() {
+
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage =
+				'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+
+ibg();
+// функция ibg=======================================================
+
 let burger = document.querySelector(".burger-menu");
 let menuBody = document.querySelector(".menu__body");
 burger.addEventListener('click', function (event){
