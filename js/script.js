7e29078da4a4aed035b27cb56cb19082028ca258
Serhii Kharchenko <kalymar7k@gmail.com>
2024-01-18 00:12:44 +0200

    end footer

--- js/script.js
+++ js/script.js
@@ -418,15 +418,42 @@ spollers();
 // ==================================================================
 
 
+// ==================================================================
+// Валидация INPUT-a=================================================
 
 
+// const subscribeInput = document.querySelector(".input");
+
+// subscribeInput.addEventListener("input", oninput);
+
+// const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+// function onInput() {
+// 	if (isEmailValid(input.value)) {
+// 		input.style.borderColor = 'green';
+// 	} else {
+// 		input.style.borderColor = 'red';
+// 	}
+// }
+
+// function isEmailValid(value) {
+// 	return EMAIL_REGEXP.test(value);
+// }
 
 
+// const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+// const input = document.querySelector('input');
 
+// function isEmailValid(value) {
+// 	return EMAIL_REGEXP.test(value);
+// }
 
+// function onInput() {
+// 	if (isEmailValid(input.value)) {
+// 		input.style.borderColor = 'green';
+// 	} else {
+// 		input.style.borderColor = 'red';
+// 	}
+// }
 
-
-
-
-
-
+// input.addEventListener('input', onInput);
